d6aade1c68bc7ace55dde9be49cef7dc092c14b0
zantaclaus <tarzuto@gmail.com>
2023-02-18 13:05:56 +0700

    ✨ Implement pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,24 @@
+import { lazy, Suspense } from 'react';
 import { Route, Routes as RRDRoutes } from 'react-router-dom';
+
+const Home = lazy(() => import('@/pages/home'));
+const Product = lazy(() => import('@/pages/product'));
+const Products = lazy(() => import('@/pages/products'));
+const Signin = lazy(() => import('@/pages/signin'));
 
 const Routes = () => {
   return (
-    <RRDRoutes>
-      <Route element={<>Home Page</>} path="/" />
-      <Route element={<>Login Page</>} path="/signin" />
-      <Route element={<>Product Lists Page</>} path="/products" />
-      <Route element={<>Product Detail Page</>} path="/products/:id" />
-      <Route element={<>Checkout Page</>} path="/checkout" />
-      <Route element={<>Cart Page</>} path="/cart" />
-      <Route element={<>History Page</>} path="/history" />
-    </RRDRoutes>
+    <Suspense fallback={<>Loadingl...</>}>
+      <RRDRoutes>
+        <Route element={<Home />} path="/" />
+        <Route element={<Signin />} path="/signin" />
+        <Route element={<Products />} path="/products" />
+        <Route element={<Product />} path="/products/:id" />
+        <Route element={<>Checkout Page</>} path="/checkout" />
+        <Route element={<>Cart Page</>} path="/cart" />
+        <Route element={<>History Page</>} path="/history" />
+      </RRDRoutes>
+    </Suspense>
   );
 };
 
