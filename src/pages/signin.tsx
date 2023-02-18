1d97e77b058fdc5ac1a75785270b9dadf8c06af8
zantaclaus <tarzuto@gmail.com>
2023-02-18 19:19:06 +0700

    🛠 Extract signin-form to section

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,16 +1,6 @@
-import { HookTextField, useHookForm } from 'mui-react-hook-form-plus';
-
-const defaultValues = { person: { username: '', password: '' } };
+import { SigninForm } from '@/sections/signin';
 
 const Signin = () => {
-  const { registerState, handleSubmit } = useHookForm({
-    defaultValues,
-  });
-
-  const onSubmit = (_data: typeof defaultValues) => {
-    console.log(_data);
-  };
-
   return (
     <div className="w-screen h-screen grid grid-cols-2">
       <div className="w-full h-full bg-blue-300"></div>
@@ -24,46 +14,7 @@ const Signin = () => {
             </p>
           </header>
 
-          <form
-            className="flex flex-col gap-4 min-w-[24rem]"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <HookTextField
-              {...registerState('person.username')}
-              textFieldProps={{
-                label: 'Username',
-                fullWidth: true,
-              }}
-              rules={{
-                required: {
-                  message: 'Username is required',
-                  value: true,
-                },
-              }}
-            />
-
-            <HookTextField
-              {...registerState('person.password')}
-              textFieldProps={{
-                label: 'Password',
-                type: 'password',
-                fullWidth: true,
-              }}
-              rules={{
-                required: {
-                  message: 'Passworo is required',
-                  value: true,
-                },
-              }}
-            />
-
-            <button
-              type="submit"
-              className="bg-black py-2.5 font-semibold text-white mt-4 rounded-full"
-            >
-              Sign in
-            </button>
-          </form>
+          <SigninForm />
         </div>
       </div>
     </div>
