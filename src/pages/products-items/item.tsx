fda78ae6c74264cb1cebc5c73f8d6889503f1b26
kuroisepr <kanittha41@gmail.com>
2023-02-18 14:53:58 +0700

    init + component setup

---
+++ src/pages/products-items/item.tsx
@@ -0,0 +1,8 @@
+import Button from '@mui/material';
+
+import { CartItemType } from '../products';
+import { Wrapper } from './item.style';
+
+type Props = {
+    item: CartItemType;
+}
--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,6 +1,37 @@
 import React from 'react';
+//import { useState } from 'react';
+import { useQuery } from 'react-query';
+
+//Component
+import Drawer from '@mui/material/Drawer';
+import { LinearProgress } from '@mui/material';
+import Grid from '@mui/material';
+import { AddShoppingCart } from '@mui/icons-material';
+import Badge from '@mui/material';
+
+import { Wrapper } from './products-items/item.style'
+
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json();
 
 const Products = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>(
+    'products',
+    getProducts
+  );
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
   return <div>Products Page</div>;
 };
 
