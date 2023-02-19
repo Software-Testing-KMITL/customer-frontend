79b8c3ba5df4a77d96e9f4229fbf3dc5595fa6eb
zantaclaus <tarzuto@gmail.com>
2023-02-19 11:41:15 +0700

    ✨ Implement Products ui

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,18 +1,14 @@
 import { ProductCard } from '@/components/product';
 import { fakeProducts } from '../services/fakedata';
 
-// const Container = styled.div`
-//   padding: 20px;
-//   display: flex;
-//   flex-wrap: wrap;
-// `;
-
 const Products = () => {
   return (
-    <div>
-      {fakeProducts.map((item, index) => (
-        <ProductCard key={index} />
-      ))}
+    <div className="pt-16 pb-32">
+      <div className="flex flex-wrap justify-between  gap-4 container mx-auto">
+        {fakeProducts.map((product, index) => (
+          <ProductCard key={index} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
