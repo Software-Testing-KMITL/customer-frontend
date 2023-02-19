241fbea4798c13738d99cb7d8b6836ced790a04d
kuroisepr <kanittha41@gmail.com>
2023-02-19 09:19:02 +0700

    Component Setup

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import { ProductCard } from '@/components/product';
+import { fakeProducts } from '../services/fakedata';
+
+// const Container = styled.div`
+//   padding: 20px;
+//   display: flex;
+//   flex-wrap: wrap;
+// `;
 
 const Products = () => {
-  return <div>Products Page</div>;
+  return (
+    <div>
+      {fakeProducts.map((item, index) => (
+        <ProductCard key={index} />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
