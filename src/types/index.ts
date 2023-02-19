f3885ead244fd1abe5e25974c8b79afa2e454e2b
Soravee Rattanaapha <parker.soravee@gmail.com>
2023-02-19 13:36:06 +0700

    🔨 api type in types index change export to all

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,10 +1,3 @@
 export type { UserType } from "./user";
 export type { ProductType, CategoryType } from "./product";
-export type {
-  SignInResquestType,
-  SignInResponseType,
-  ProductListRequestType,
-  ProductListResponseType,
-  ProductDetailRequestType,
-  ProductDetailResponseType,
-} from "./api";
+export * from "./api";
