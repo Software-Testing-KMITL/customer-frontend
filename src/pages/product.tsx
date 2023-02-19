6f063bfa7651f29d9571646c1421bab70590ae1e
JusAeng <justo_play@hotmail.com>
2023-02-19 13:12:23 +0700

    split detail component to section

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,55 +1,35 @@
 import React, { useEffect, useState } from "react";
-import { ProductQualtity } from "@/sections/product";
 import { ProductType } from "@/types";
 import { AuthGuard } from "@/components/auth";
 import { getProductById } from "@/api";
+import { useParams } from "react-router-dom";
+import ProductDetail from "@/sections/product/ProductDetail";
 
 const Product = () => {
-  const data: ProductType = {
-    id: "",
+  let temp: ProductType = {
+    _id: "",
     name: "Product Name",
     price: 44.99,
     amount: 9,
     description:
       "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cumque omnis consequuntur vitae, qui voluptas quas excepturi officiis dignissimos praesentium? Perferendis consequatur dolorum facilis delectus reprehenderit! Facere dicta ea pariatur nemo.",
-    category: "tops",
+    category: ["tops"],
     picture: "",
   };
 
-  const [want, setWant] = useState(1);
-  // console.log("want on page =", want);
+  const handleData = (res: any) => {
+    console.log(res);
+  };
+  const params = useParams();
+  // console.log(params);
 
-  const handleResponse = () => {};
-
-  // useEffect(() => {
-  //   getProductById();
-  // }, []);
+  useEffect(() => {
+    getProductById({ productId: params.id || "" }, handleData);
+  }, []);
 
   return (
     <AuthGuard>
-      <div className="h-[100vh] grid place-items-center">
-        <div className="container grid grid-cols-2 gap-[20px]">
-          <section className="flex justify-center ">
-            <div className="bg-black rounded-[20px] w-[500px] h-[500px]"></div>
-          </section>
-          <section>
-            <h2 className="text-5xl font-bold mb-[30px]">{data.name}</h2>
-            <p className="pr-[25%]">{data.description}</p>
-            <h3 className="text-2xl text-orange-400 font-semibold my-[20px]">
-              $ {data.price}
-            </h3>
-            <ProductQualtity remain={data.amount} setWant={setWant} />
-            <div className="flex gap-[30px]">
-              <button className="bg-orange-100 rounded-[7px] text-xl p-[12px] border-[2px] border-orange-400 text-orange-400">
-                Add to cart
-              </button>
-              <button className="bg-orange-400 rounded-[7px] text-xl p-[12px] text-white">
-                Buy now
-              </button>
-            </div>
-          </section>
-        </div>
-      </div>
+      <ProductDetail data={temp} />
     </AuthGuard>
   );
 };
